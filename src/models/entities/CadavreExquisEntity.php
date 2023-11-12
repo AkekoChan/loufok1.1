62e28e60175f81fea7d673ab69c135e21c0aea8b
Maxence <maxence.gvr@gmail.com>
2023-11-12 20:05:51 +0100

    Prepared Periode Class

--- src/models/entities/CadavreExquisEntity.php
+++ src/models/entities/CadavreExquisEntity.php
@@ -1,6 +1,8 @@
 <?php
     namespace App\Models\Entities;
 
+    use App\Models\Util\Periode;
+
     class CadavreExquisEntity {
         public int $id_game;
         public string $title;
@@ -9,5 +11,12 @@
         public int $nb_contribution;
         public int $nb_like;
         public int $id_admin;
+
+        public Periode $periode;
+
+        public function __construct()
+        {
+            $this->periode = new Periode($this->date_start, $this->date_end);
+        }
     }
 ?>
---
+++ src/models/util/Periode.php
@@ -0,0 +1,10 @@
+<?php
+    namespace App\Models\Util;
+
+    class Periode {
+        public function __construct()
+        {
+            
+        }
+    }
+?>
