ccd774ad88f9e58438fa9418947fc60c1dca5f0e
Maxence <maxence.gvr@gmail.com>
2023-11-15 23:41:48 +0100

    Update RandContributionModel.php

--- src/models/RandContributionModel.php
+++ src/models/RandContributionModel.php
@@ -3,7 +3,8 @@
 
     use App\Service\Database\Model;
     use App\Service\Database\DatabaseManager;
-
+    
+    use App\Models\Entities\ContributionEntity;
     use App\Models\Entities\RandContributionEntity;
 
     class RandContributionModel extends Model {
@@ -11,53 +12,36 @@
         public string $entity = RandContributionEntity::class;
 
         // Cette fonction récupère la contribution aléatoire correspondant à l'utilisateur et au cadavre actuel ou la créer
-        public function getRandomContribution (int $user_id, int $current_cadavre_id) : RandContributionEntity {
-            $sql = "SELECT *
-            FROM {$this->table}
-            WHERE id_user = :user_id AND id_cadavre_exquis = :cadavre_id
-            LIMIT 1;";
-
-            $sth = DatabaseManager::query($sql, [
-                ":user_id" => $user_id,
-                ":cadavre_id" => $current_cadavre_id
-            ]);
-
-            if ($sth && $sth->rowCount()) {
-                // $res->fetchAll(DatabaseManager::getInstance()::FETCH_CLASS, $this->entity);
-                $sth->setFetchMode(DatabaseManager::getInstance()::FETCH_CLASS, $this->entity);
-                return $sth->fetch();
-            } else {
-                return $this->assignRandomContribution($user_id, $current_cadavre_id);
-            }
-        }
-
-        // Cette fonction assigne une contribution aléatoire à l'utilisateur en fonction du cadavre actuel
-        private function assignRandomContribution (int $user_id, int $current_cadavre_id) : RandContributionEntity {
+        public function getRandomContribution (int $user_id, int $current_cadavre_id) : ContributionEntity {
             $contribution_table = ContributionModel::getTableName();
+            $contribution_entity = ContributionEntity::class;
 
-            $sql = "INSERT INTO {$this->table} (id_user, id_cadavre_exquis, num_contribution)
-                        VALUES (
-                            :user_id,
-                            :cadavre_id,
-                            (SELECT id_contribution FROM {$contribution_table} WHERE id_cadavre_exquis = :cadavre_id ORDER BY RAND() LIMIT 1)
-                        );";
-                
+            $sql = "SELECT
+                c.*
+            FROM
+                {$this->table} rc
+            JOIN
+                {$contribution_table} c ON rc.num_contribution = c.id_contribution
+            WHERE
+                rc.id_user = :user_id
+                AND rc.id_cadavre_exquis = :cadavre_exquis_id
+            LIMIT 1;
+
+            INSERT INTO {$this->table} (id_user, id_cadavre_exquis, num_contribution)
+            SELECT
+                :user_id,
+                :cadavre_exquis_id,
+                (SELECT id_contribution FROM {$contribution_table} WHERE id_cadavre_exquis = :cadavre_exquis_id ORDER BY RAND() LIMIT 1)
+            WHERE NOT EXISTS (
+                SELECT 1 FROM {$this->table} WHERE id_user = :user_id AND id_cadavre_exquis = :cadavre_exquis_id
+            );";
+
             $sth = DatabaseManager::query($sql, [
-                    ":user_id" => $user_id,
-                    ":cadavre_id" => $current_cadavre_id
-            ]);
-
-            $select = "SELECT *
-            FROM {$this->table}
-            WHERE id_user = :user_id AND id_cadavre_exquis = :cadavre_id
-            LIMIT 1;";
-
-            $sth = DatabaseManager::query($select, [
                 ":user_id" => $user_id,
-                ":cadavre_id" => $current_cadavre_id
+                ":cadavre_exquis_id" => $current_cadavre_id
             ]);
 
-            $sth->setFetchMode(DatabaseManager::getInstance()::FETCH_CLASS, $this->entity);
+            $sth->setFetchMode(DatabaseManager::getInstance()::FETCH_CLASS, $contribution_entity);
             return $sth->fetch();
         }
     }
