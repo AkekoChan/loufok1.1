85af83df48d91afb60648daa98eade47285e469c
Akeko <artapp.theo@gmail.com>
2023-11-26 12:45:23 +0100

    deleting all content from branch

--- src/models/entities/RandContributionEntity.php
+++
@@ -1,9 +0,0 @@
-<?php
-    namespace App\Entities;
-
-    class RandContributionEntity {
-        public int $num_contribution;
-        public int $id_user;
-        public int $id_cadavre_exquis;
-    }
-?>
