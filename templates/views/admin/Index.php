f0766bb160466055bc2a6594d7aca29ad0a1b5f7
Maxence <maxence.gvr@gmail.com>
2023-11-28 01:19:08 +0100

    Updated controls and store

--- templates/views/admin/Index.php
+++ templates/views/admin/Index.php
@@ -24,7 +24,7 @@ class Index extends Template
         <section class="admin-cadavers">
           <div class="admin-cadavers__info right-fade">
             <h2 class="admin-cadavers__title bigger">
-              Consultez les Cadavres exquis
+              Consultez les Cadavres Exquis
             </h2>
             <p class="admin-cadavers__description center">
               Consultez tous les cadavres exquis que vous avez créés, ceux qui
